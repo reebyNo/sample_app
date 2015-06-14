0b6d715aa94ce89821cae288ce54d97e77eec4a1
= <moequalitycontrol@gmail.com>
2015-06-14 19:54:03 -0400

    add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+# Returns the full title on a per-page basis
+def full_title(page_title = '')
+  base_title = "Ruby on Rails Sample"
+  return base_title if page_title.empty?
+  return page_title + " | " + base_title
+end
+
+user = { :name => "Timmy Tully", :email => "ttully514@gmail.com}" }
+
+user = { name: "tim", email: "fun@gmail.com" }
+
+class Word < String
+  def palindrome?
+    self == self.reverse
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Sample"
+    assert_select "title", "Ruby on Rails Sample"
   end
 
   test "should get help" do
